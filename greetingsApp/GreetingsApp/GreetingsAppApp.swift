83aef8fcd802a310cf54c0039c9723de0eae6934
salimzhanova <salimzhanova@kolesa.kz>
2026-03-24 15:28:53 +0500

    greetings app added

---
+++ greetingsApp/GreetingsApp/GreetingsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingsAppApp.swift
+//  GreetingsApp
+//
+//  Created by kamila on 24.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct GreetingsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
