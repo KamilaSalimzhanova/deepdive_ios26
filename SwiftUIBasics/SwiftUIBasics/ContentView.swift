7dab720ebe4e970e7baa5f6263c509689fa27587
salimzhanova <salimzhanova@kolesa.kz>
2026-03-28 18:18:36 +0500

    beautiful rotation

--- SwiftUIBasics/SwiftUIBasics/ContentView.swift
+++ SwiftUIBasics/SwiftUIBasics/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    var text: String {
+        isTapped
+        ? "Swift UI Basics"
+        : "To know that u know and what u do not know, that is true knowledge"
+    }
+    var rotationAngle: CGFloat {
+        isTapped ? .zero : 360.0
+    }
+    
+    @State private var colorList: [Color] = [.red, .purple, .green, .pink]
+    @State private var isTapped: Bool = false
+        
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(
+                colors: colorList,
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+            .opacity(0.5)
+            .ignoresSafeArea()
+            
+            Text(text)
+                .font(isTapped ? .largeTitle : .title)
+                .fontWeight(.semibold)
+                .foregroundStyle(isTapped ? .black : .red)
+                .multilineTextAlignment(.center)
+                .rotation3DEffect(.degrees(rotationAngle), axis: (x: 0, y: 1, z: 0))
+                .padding()
         }
-        .padding()
+        .onTapGesture {
+            withAnimation(.easeIn(duration: 1.5)){
+                isTapped.toggle()
+                colorList.shuffle()
+            }
+        }
     }
 }
 
