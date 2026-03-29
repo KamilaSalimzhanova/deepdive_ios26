bd4332975713b934a4f0d22b4b5168501cab3ccf
salimzhanova <salimzhanova@kolesa.kz>
2026-03-29 21:52:04 +0500

    created one more new project for liquid glass

---
+++ LiquidGlassDemo/LiquidGlassDemo/LiquidGlassDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidGlassDemoApp.swift
+//  LiquidGlassDemo
+//
+//  Created by kamila on 29.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct LiquidGlassDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
