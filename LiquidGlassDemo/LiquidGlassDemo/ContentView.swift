81974c6cebf9e8328a7c162bfb351009b8c5f34f
salimzhanova <salimzhanova@kolesa.kz>
2026-03-29 22:29:13 +0500

    liquid glass view

--- LiquidGlassDemo/LiquidGlassDemo/ContentView.swift
+++ LiquidGlassDemo/LiquidGlassDemo/ContentView.swift
@@ -7,15 +7,102 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+enum PersonType: String, CaseIterable, Identifiable {
+    case female, male, skater, footballer
+    
+    var id: String { rawValue }
+    
+    var color: Color {
+        switch self {
+        case .female:
+            return .pink
+        case .male:
+            return .blue
+        case .skater:
+            return .green
+        case .footballer:
+            return .orange
         }
-        .padding()
+    }
+    
+    var systemImage: String {
+        switch self {
+        case .female:
+            "figure.stand.dress"
+        case .male:
+            "figure.stand"
+        case .skater:
+            "figure.skateboarding"
+        case .footballer:
+            "figure.american.football"
+        }
+    }
+}
+
+struct ImageModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .frame(width: 60, height: 60)
+            .glassEffect(.regular.tint(.red.opacity(0.7)))
+    }
+}
+
+struct ContentView: View {
+    var hStackSpacing: CGFloat {
+        morph ? 30 : 0
+    }
+    
+    @Namespace private var personNamespace
+    @State private var selectedPerson: PersonType = .female
+    @State private var morph = false
+    
+    var body: some View {
+        ZStack {
+            Image(.dog)
+                .resizable()
+                .scaledToFill()
+                .opacity(0.8)
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("Hello world!")
+                    .padding()
+                    .glassEffect()
+                
+                GlassEffectContainer(spacing: 30) {
+                    HStack(spacing: hStackSpacing) {
+                        Image(systemName: "scribble")
+                            .modifier(ImageModifier())
+                        Image(systemName: "eraser")
+                            .modifier(ImageModifier())
+                    }
+                    .onTapGesture {
+                        withAnimation(.bouncy) {
+                            morph.toggle()
+                        }
+                    }
+                }
+                
+                GlassEffectContainer {
+                    HStack(spacing: 24) {
+                        ForEach(PersonType.allCases) { person in
+                            Button {
+                                selectedPerson = person
+                            } label: {
+                                Image(systemName: person.systemImage)
+                                    .bold()
+                                    .foregroundStyle(selectedPerson == person ? person.color : .secondary)
+                            }
+                            .buttonStyle(.glassProminent)
+                            .glassEffectUnion(id: "test", namespace: personNamespace)
+                        }
+                    }
+                    .font(.largeTitle)
+                    .tint(.indigo.opacity(0.2))
+                }
+            }
+        }
     }
 }
 
