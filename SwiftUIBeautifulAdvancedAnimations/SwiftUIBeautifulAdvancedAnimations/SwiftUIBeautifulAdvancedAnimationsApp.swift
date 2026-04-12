92f43476b65ef89edcb66a953672996ce51379c6
salimzhanova <salimzhanova@kolesa.kz>
2026-04-12 22:31:30 +0500

    created a poject for animations

---
+++ SwiftUIBeautifulAdvancedAnimations/SwiftUIBeautifulAdvancedAnimations/SwiftUIBeautifulAdvancedAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBeautifulAdvancedAnimationsApp.swift
+//  SwiftUIBeautifulAdvancedAnimations
+//
+//  Created by kamila on 12.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBeautifulAdvancedAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
