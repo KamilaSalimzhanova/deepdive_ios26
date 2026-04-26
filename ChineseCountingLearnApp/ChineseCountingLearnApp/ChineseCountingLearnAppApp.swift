c763f0b632bdc8a578fb57080a36c8e0455416f3
salimzhanova <salimzhanova@kolesa.kz>
2026-04-26 13:57:43 +0500

    setting up the project

---
+++ ChineseCountingLearnApp/ChineseCountingLearnApp/ChineseCountingLearnAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChineseCountingLearnAppApp.swift
+//  ChineseCountingLearnApp
+//
+//  Created by kamila on 26.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct ChineseCountingLearnAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
